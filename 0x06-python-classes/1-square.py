d8feb30a7c7e9559294f7f1457bef068d3c7c00a
Azizaazu <aynaou.aziza98@gmail.com>
2023-08-30 03:37:47 -0700

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-
+"""Module square."""
 class Square:
-
+"""Represent a square."""
 
 
     def __init__(self, size=0):
+        """Constructor.
 
-
+        Args:
+            size: length
+        """
 
         self.__size = size
