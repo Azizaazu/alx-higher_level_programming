3a95104bbef1bafa9b1ddf6ebeb680c51a6e7560
Azizaazu <aynaou.aziza98@gmail.com>
2023-08-30 08:09:51 -0700

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+"""Square module."""
+
 
 class Square:
-
+    """Defines a square."""
 
     def __init__(self, size=0):
+        """Constructor.
 
+        Args:
+            size: Length of a side of the square.
 
-        try:
-            self.__size = size
-            if size < 0:
-                raise ValueError
-            if type(size) is not int:
-                raise TypeError
-        except TypeError:
-            raise TypeError("size must be an integer")
-        except ValueError:
-            raise ValueError("size must be >= 0")
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-
+"""Square module."""
 class Square:
-
+    """Represent a square"""
 
     def __init__(self, size=0):
+        """Constructor.
 
+        Args:
+            size: Length of a side of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
 
         try:
             self.__size = size
@@ -18,5 +26,9 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """Area of this square.
 
+        Returns:
+            The size squared.
+        """
         return self.__size * self.__size
