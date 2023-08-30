53d642b6b571906617c05ce4563acac705e178f1
Azizaazu <aynaou.aziza98@gmail.com>
2023-08-30 08:14:41 -0700

    update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,78 +1,75 @@
-#include <stdio.h>
 #include <Python.h>
 
-void print_python_list(PyObject *p);
-
-/**
- * print_python_bytes - Prints bytes
- * @p: python
- * Return: nothing
- *
- */
-
-
-void print_python_bytes(PyObject *p)
-{
-	char *str;
-	long int s, i, lmt;
-
-	printf("[.] bytes object info\n");
-	if (!PyBytes_Check(p))
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-		return;
-	}
-
-	s = ((PyVarObject *)(p))->ob_size;
-	str = ((PyBytesObject *)p)->ob_sval;
-
-	printf("  size: %ld\n", s);
-	printf("  trying string: %s\n", str);
-
-	if (s >= 10)
-		lmt = 10;
-	else
-		lmt = s + 1;
-
-	printf("  first %ld bytes:", lmt);
-
-	for (i = 0; i < lmt; i++)
-		if (str[i] >= 0)
-			printf(" %02x", str[i]);
-		else
-			printf(" %02x", 256 + str[i]);
-
-	printf("\n");
-}
-
-
-
-/**
- * print_python_list - Prints list
- * @p: Python
- * Return: nothing
- *
- */
-
-
+// Function to print information about a Python list
 void print_python_list(PyObject *p)
 {
-	long int s, i;
-	PyListObject *lst;
-	PyObject *object;
+    if (!PyList_Check(p))
+    {
+        printf("Error: Not a Python list.\n");
+        return;
+    }
 
-	s = ((PyVarObject *)(p))->ob_size;
-	lst = (PyListObject *)p;
+    Py_ssize_t size = PyList_Size(p);
 
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", s);
-	printf("[*] Allocated = %ld\n", lst->allocated);
+    printf("[*] Python List Info\n");
+    printf("[*] Size of the Python List = %zd\n", size);
 
-	for (i = 0; i < s; i++)
-	{
-		object = ((PyListObject *)p)->ob_item[i];
-		printf("Element %ld: %s\n", i, ((object)->ob_type)->tp_name);
-		if (PyBytes_Check(obj))
-			print_python_bytes(object);
-	}
+    for (Py_ssize_t i = 0; i < size; i++)
+    {
+        PyObject *item = PyList_GetItem(p, i);
+        PyObject *item_str = PyObject_Str(item);
+        const char *item_cstr = PyUnicode_AsUTF8(item_str);
+        printf("Element %zd: %s\n", i, item_cstr);
+        Py_XDECREF(item_str);
+    }
+}
+
+// Function to print information about a Python bytes object
+void print_python_bytes(PyObject *p)
+{
+    if (!PyBytes_Check(p))
+    {
+        printf("Error: Not a Python bytes object.\n");
+        return;
+    }
+
+    Py_ssize_t size = PyBytes_Size(p);
+    const char *data = PyBytes_AsString(p);
+
+    printf("[.] bytes object info\n");
+    printf("  size: %zd\n", size);
+    printf("  trying string: %s\n", data);
+
+    printf("  first %zd bytes: ", (size <= 10) ? size : 10);
+    for (Py_ssize_t i = 0; i < ((size <= 10) ? size : 10); i++)
+    {
+        printf("%02x", (unsigned char)data[i]);
+        if (i < 9) printf(" ");
+    }
+    printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+    // Initialize the Python interpreter
+    Py_Initialize();
+
+    // Create a Python list and print its info
+    PyObject *py_list = PyList_New(3);
+    PyList_SetItem(py_list, 0, PyLong_FromLong(42));
+    PyList_SetItem(py_list, 1, PyUnicode_DecodeUTF8("hello", 5, "strict"));
+    PyList_SetItem(py_list, 2, PyFloat_FromDouble(3.14159));
+    print_python_list(py_list);
+
+    // Create a Python bytes object and print its info
+    const char data[] = {0x01, 0x23, 0x45, 0x67, 0x89};
+    PyObject *py_bytes = PyBytes_FromStringAndSize(data, sizeof(data));
+    print_python_bytes(py_bytes);
+
+    // Clean up and finalize the Python interpreter
+    Py_XDECREF(py_list);
+    Py_XDECREF(py_bytes);
+    Py_Finalize();
+
+    return 0;
 }
--- 0x04-python-more_data_structures/Python.h
+++ 0x04-python-more_data_structures/Python.h
@@ -1,7 +1,41 @@
-#ifndef Python_H
-#define Python_H
+#ifndef PYTHON_H
+#define PYTHON_H
+
+typedef struct _object {
+    PyObject_HEAD
+} PyObject;
+
+typedef struct {
+    PyObject_VAR_HEAD
+    Py_ssize_t ob_size;
+} PyVarObject;
+
+typedef struct {
+    PyObject_VAR_HEAD
+    PyObject *ob_item[1];
+} PyListObject;
+
+typedef struct {
+    PyObject_VAR_HEAD
+    char *ob_sval;
+} PyBytesObject;
+
+
+typedef long Py_ssize_t;
+
+// Define PyObject_HEAD and related macros
+#define PyObject_HEAD PyVarObject ob_base;
+#define PyObject_VAR_HEAD Py_ssize_t ob_refcnt; PyTypeObject *ob_type;
+
+// Define PyObject types (placeholders for demonstration purposes)
+typedef struct _typeobject PyTypeObject;
+
+// Define constants (placeholders for demonstration purposes)
+#define PyList_Check(op) 1
+#define PyBytes_Check(op) 1
 
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
+
 
 #endif
