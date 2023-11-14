cae4c9c4d514974988296efb74fe0cb198adcdfa
Azizaazu <aynaou.aziza98@gmail.com>
2023-11-14 13:58:41 -0800

    update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-const { list } = require('./100-data');
-const newList = list.map((value, index) => value * index);
+const list = require('./100-data.js').list;
 
-console.log("Initial List:", list);
-console.log("New List:", newList);
+console.log(list);
+console.log(list.map((item, index) => item * index));
