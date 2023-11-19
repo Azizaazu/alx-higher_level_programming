b01243143fcd71175045fb5d87a8d5d0d58d0982
Azizaazu <aynaou.aziza98@gmail.com>
2023-11-19 01:51:02 -0800

    update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+""" lists all cities from the database hbtn_0e_4_usa """
 
 import MySQLdb
 import sys
@@ -7,12 +7,11 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
     c = db.cursor()
-    match = sys.argv[4]
+    c.execute(""" SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id=cities.state_id """)
 
-    c.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
     rows = c.fetchall()
-
     for row in rows:
         print(row)
     c.close()
